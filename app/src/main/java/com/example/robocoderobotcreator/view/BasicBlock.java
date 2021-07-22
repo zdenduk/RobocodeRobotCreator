b736fe05ea2bc973358b49cfc206d8a3821ea2c6
Zdenek David <zdenek.david@educanet.cz>
2021-07-22 16:36:21 +0200

    Revert to 5231fa5737f65b92cbe65aa5b60e8218dc72cb6d

--- app/src/main/java/com/example/robocoderobotcreator/view/BasicBlock.java
+++ app/src/main/java/com/example/robocoderobotcreator/view/BasicBlock.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.util.AttributeSet;
-import android.view.Gravity;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,14 +12,8 @@ import androidx.core.content.ContextCompat;
 import com.asitrack.fontawesome2.FontAwesome.FontAwesomeUtils;
 import com.example.robocoderobotcreator.R;
 import com.example.robocoderobotcreator.model.Block;
-import com.example.robocoderobotcreator.model.events.OnScannedRobot;
 import com.example.robocoderobotcreator.model.events.Run;
-import com.example.robocoderobotcreator.model.events.WhileBlock;
-import com.example.robocoderobotcreator.model.movement.Ahead;
-import com.example.robocoderobotcreator.model.movement.Back;
 import com.example.robocoderobotcreator.model.weapons.Fire;
-import com.example.robocoderobotcreator.model.weapons.TurnGunLeft;
-import com.example.robocoderobotcreator.model.weapons.TurnGunRight;
 
 public class BasicBlock extends androidx.appcompat.widget.AppCompatTextView {
 
@@ -29,8 +22,8 @@ public class BasicBlock extends androidx.appcompat.widget.AppCompatTextView {
     public BasicBlock(@NonNull Context context, Block blockRef) {
         super(context);
         this.blockRef = blockRef;
-
-        initBlockViewParameters(context);
+        setColor(context);
+        setText(context);
     }
 
     public BasicBlock(@NonNull Context context, @Nullable AttributeSet attrs, Block blockRef) {
@@ -45,14 +38,6 @@ public class BasicBlock extends androidx.appcompat.widget.AppCompatTextView {
         setColor(context);
     }
 
-    private void initBlockViewParameters(@NonNull Context context) {
-        setColor(context);
-        setText(context);
-        setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-        setWidth(128);
-        setHeight(128);
-        setTextColor(Color.WHITE);
-    }
 
     private void setColor(Context context) {
         switch (blockRef.getCategory()) {
@@ -74,21 +59,10 @@ public class BasicBlock extends androidx.appcompat.widget.AppCompatTextView {
     private void setText(Context context) {
         //IconHelper.resolveIcon(blockRef.getClass(), context, this);
         if (Run.class.equals(blockRef.getClass())) {
-            setText("RUN");//FontAwesomeUtils.changeFontAwesomeForRegularIconColor(context, this, "\uf04b", Color.WHITE);
-        } else if (Fire.class.equals(blockRef.getClass())) {
-            setText("FIRE");//FontAwesomeUtils.changeFontAwesomeForRegularIconColor(context, this, "\uf05b", Color.WHITE);
-        } else if (TurnGunLeft.class.equals(blockRef.getClass())) {
-            setText("TURNGUNLEFT");
-        } else if (TurnGunRight.class.equals(blockRef.getClass())) {
-            setText("TURNGUNRIGHT");
-        } else if (OnScannedRobot.class.equals(blockRef.getClass())) {
-            setText("ONSCANNEDROBOT");
-        } else if (WhileBlock.class.equals(blockRef.getClass())) {
-            setText("WHILE");
-        } else if (Ahead.class.equals(blockRef.getClass())) {
-            setText("AHEAD");
-        } else if (Back.class.equals(blockRef.getClass())) {
-            setText("BACK");
+            FontAwesomeUtils.changeFontAwesomeForRegularIconColor(context, this, "\uf04b", Color.WHITE);
+        }
+        if (Fire.class.equals(blockRef.getClass())) {
+            FontAwesomeUtils.changeFontAwesomeForRegularIconColor(context, this, "\uf05b", Color.WHITE);
         }
     }
 
